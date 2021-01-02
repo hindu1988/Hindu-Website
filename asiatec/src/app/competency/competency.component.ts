88c6424f72fc9e3c4da234db0093744787b30839
hinduhari <hinduhari1234@gmail.com>
2021-01-02 13:51:55 +0800

    Create competency.component.ts

---
+++ asiatec/src/app/competency/competency.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-competency',
+  templateUrl: './competency.component.html',
+  styleUrls: ['./competency.component.css']
+})
+export class CompetencyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
