03fd97861eb5ce23229433c07364fd59b1f1f69d
hinduhari <hinduhari1234@gmail.com>
2021-01-02 13:54:18 +0800

    Create projectreference.component.ts

---
+++ asiatec/src/app/projectreference/projectreference.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-projectreference',
+  templateUrl: './projectreference.component.html',
+  styleUrls: ['./projectreference.component.css']
+})
+export class ProjectreferenceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
